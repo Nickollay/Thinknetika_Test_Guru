94f95f849a8fb8aae0288a848ed4efb1edd45d0a
nickollay <nickollaysokolov@gmail.com>
2020-08-19 09:11:38 +0300

    save point

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'tests#index'
 
+
   resources :tests do
-    resources :questions, shallow: true, except: :index
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
 end
