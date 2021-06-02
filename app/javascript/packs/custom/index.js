c77f743bc62be6a0b1a753e04e0ad1f5a69eb10e
NickSokolov <nick.s@railsmuffin.com>
2021-06-02 21:26:59 +0300

    Half result

--- app/javascript/packs/custom/index.js
+++ app/javascript/packs/custom/index.js
@@ -1,3 +1,7 @@
 require("utilities")
-require("utilities")
-require("global")
+
+const signUpForm = document.getElementById('user-sign-up')
+if(signUpForm.length) new passwordConfirmation(signUpForm)
+
+const tablesToSort = document.getElementsByClassName('sortable-table')
+if(tablesToSort.length) tablesToSort.forEach( table => { new sortTableRowsByTitle(table) })
