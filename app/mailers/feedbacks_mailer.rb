6a8c3501c973aafc2f5b87cc48159bae19270fc5
nickollay <nickollaysokolov@gmail.com>
2021-06-16 23:04:02 +0300

    Add views & routes for feedback

--- app/mailers/feedbacks_mailer.rb
+++ app/mailers/feedbacks_mailer.rb
@@ -1,9 +1,9 @@
 class FeedbacksMailer < ApplicationMailer
   def feedback(feedback, user)
     @feedback = feedback
-    @user = user
-    admin = Admin.where('email LIKE ?', 'vvv').take
+    @user = user&.email || t('.stranger')
+    admin_email = ENV.fetch('ADMIN_EMAIL')
 
-    mail to: admin.email, subject: t('.subject')
+    mail to: admin_email, subject: t('.subject')
   end
 end
